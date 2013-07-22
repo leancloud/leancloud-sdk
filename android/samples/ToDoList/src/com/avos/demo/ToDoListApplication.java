dfd1861e61e9b5fd4935e9ca394260914fc2f0cc
zengzhu <zeng.zhu@gmail.com>
2013-07-22 18:37:25 +0800

    enable public read write access.

--- android/samples/ToDoList/src/com/avos/demo/ToDoListApplication.java
+++ android/samples/ToDoList/src/com/avos/demo/ToDoListApplication.java
@@ -17,7 +17,8 @@ public class ToDoListApplication extends Application {
 		ParseUser.enableAutomaticUser();
 		ParseACL defaultACL = new ParseACL();
 		// Optionally enable public read access.
-		// defaultACL.setPublicReadAccess(true);
+		defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
 		ParseACL.setDefaultACL(defaultACL, true);
 	}
 
