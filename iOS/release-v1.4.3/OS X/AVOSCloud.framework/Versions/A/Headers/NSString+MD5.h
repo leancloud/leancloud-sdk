04107b9b4a8a68394b3ac3a8d46bff7e4d28f17b
Travis <travis@avos.com>
2013-10-31 18:29:50 +0800

    append OSX

---
+++ iOS/release-v1.4.3/OS X/AVOSCloud.framework/Versions/A/Headers/NSString+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString+MD5.h
+//  paas
+//
+//  Created by Summer on 13-3-22.
+//  Copyright (c) 2013年 AVOS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+- (NSString *)MD5String;
+
+@end
