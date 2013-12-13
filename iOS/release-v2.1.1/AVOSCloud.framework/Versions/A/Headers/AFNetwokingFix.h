6d79886c3ebb76d02b1aa12ae0a6ac3f34a34777
Jiangcheng Wu <jwu@avos.com>
2013-12-13 15:00:44 +0800

    [JENKINS] iOS sdk version v2.1.1

---
+++ iOS/release-v2.1.1/AVOSCloud.framework/Versions/A/Headers/AFNetwokingFix.h
@@ -0,0 +1,24 @@
+//
+//  AFNetwokingFix.h
+//  paas
+//
+//  Created by Travis on 13-12-9.
+//  Copyright (c) 2013年 AVOS. All rights reserved.
+//
+
+#import "AVNetworking.h"
+
+#ifndef _AFNETWORKING_
+    #define _AFNETWORKING_
+
+    typedef AVHTTPClient AFHTTPClient;
+    typedef AVHTTPRequestOperation AFHTTPRequestOperation;
+    typedef AVJSONRequestOperation AFJSONRequestOperation;
+    typedef AVXMLRequestOperation AFXMLRequestOperation;
+    typedef AVPropertyListRequestOperation AFPropertyListRequestOperation;
+    typedef AVPropertyListRequestOperation AFPropertyListRequestOperation;
+    typedef AVHTTPClient AFHTTPClient;
+    typedef AVImageRequestOperation AFImageRequestOperation;
+
+#endif
+
