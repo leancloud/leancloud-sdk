7348be3277901500e7969a414a3d2c85bae84023
Travis <travis@avos.com>
2013-12-23 16:15:38 +0800

    添加统计Demo

---
+++ iOS/samples/AVOSDemo/AVOSDemo/AVAnalyticsBasic.h
@@ -0,0 +1,13 @@
+//
+//  AVAnalyticsBasic.h
+//  AVOSDemo
+//
+//  Created by Travis on 13-12-23.
+//  Copyright (c) 2013年 AVOS. All rights reserved.
+//
+
+#import "Demo.h"
+
+@interface AVAnalyticsBasic : Demo
+
+@end
