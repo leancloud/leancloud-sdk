4d7209eb27c304c950cd85eec0ea10efa3050af6
freewing99 <wujun19890209@gmail.com>
2014-05-26 11:58:32 +0800

    add demo source code of unity
    by wujun

---
+++ unity3d/samples/AVOSCloudUnityQuickStartDemo_v1.0.0_beta/QuickStartDemo/Assets/AVOSCloudTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using AVOSCloud;
+using System.Threading.Tasks;
+
+public class AVOSCloudTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+
+	void Update () {
+
+	}
+
+	private string msg=string.Empty;
+	void OnGUI()
+	{
+		GUI.Label (new Rect (270, 50, 200, 80), msg);
+
+		if (GUI.Button (new Rect (50, 50, 200, 80), "添加GameScore"))
+		{
+			AVObject gameScore =new AVObject("GameScore");
+			gameScore["score"] = 1337;
+			gameScore["playerName"] = "Neal Caffrey";
+			gameScore.SaveAsync().ContinueWith(t=>
+			                                   {
+				if(!t.IsFaulted)
+				{
+					msg="保存成功";
+				}
+			});
+		}
+	}
+}
