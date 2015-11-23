42c9f3fa4bae15cac149213054f79c382750f20b
Tianyong Tang <tang3w@gmail.com>
2015-11-23 22:26:27 +0800

    [JENKINS] iOS sdk version v3.1.6.5

---
+++ iOS/release-v3.1.6.5/Dynamic/AVOSCloudIM.framework/Headers/AVOSCloudIM.h
@@ -0,0 +1,28 @@
+//
+//  AVOSCloudIM.h
+//  AVOSCloudIM
+//
+//  Created by Qihe Bian on 12/4/14.
+//  Copyright (c) 2014 LeanCloud Inc. All rights reserved.
+//
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVOSCloudIM/PublicHeader.h>
+
+// Public headers
+#import "AVIMCommon.h"
+#import "AVIMClient.h"
+#import "AVIMConversation.h"
+#import "AVIMKeyedConversation.h"
+#import "AVIMConversationQuery.h"
+#import "AVIMConversationUpdateBuilder.h"
+#import "AVIMMessage.h"
+#import "AVIMTypedMessage.h"
+#import "AVIMTextMessage.h"
+#import "AVIMImageMessage.h"
+#import "AVIMAudioMessage.h"
+#import "AVIMVideoMessage.h"
+#import "AVIMLocationMessage.h"
+#import "AVIMFileMessage.h"
+#import "AVIMSignature.h"
+#import "AVIMUserOptions.h"
